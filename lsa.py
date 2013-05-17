ce896f7cf4d9fb29355ce68e174b897d8b010a80
BenDaizer <albenhenni@gmail.com>
2013-05-17 17:47:22 +0200

    corrected wrong tfidf

--- lsa.py
+++ lsa.py
@@ -3,9 +3,15 @@
 import numpy as np
 from math import *
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def multiply(*args):
-    """takes custom number of numpy arrays and multiplies them"""
+    """takes custom number of numpy arrays and multiplies them
+
+    Arguments:
+    take as many numpy arrays as necessary
+    """
     i = 0
     res = 1
     while i < len(args):
@@ -23,7 +29,7 @@ def cosine(x, y):
 def build_terms(docs, stopwords):
     """build dictionary of words from docs, ignoring list of stopwords """
     terms = list(set([item.lower() for s in docs for item in s.split(" ")
-                  if item.lower() not in stopwords]))
+                      if item.lower() not in stopwords]))
     terms.sort()
     terms = dict((key, value) for (value, key) in enumerate(terms))
     return terms
@@ -40,22 +46,52 @@ def build_M(terms, docs):
 
     for i, doc in enumerate(docs_split):
         for term in doc:
-            if term in terms: M[terms.get(term), i] += 1
+            if term in terms:
+                M[terms.get(term), i] += 1
     return M
 
 
 def tfidf(M):
-    """take matrix term-doc with frequencies and normalize with tf-idf instead"""
+    """take matrix term-doc with frequencies and normalize with tf-idf instead
+
+    Arguments:
+    M : numpy 2d float array
+    """
+
+    return tf(M)*idf(M)
+
+
+def idf(M):
+    """take matrix term-doc with frequencies and normalize with tf-idf instead
+
+    Arguments:
+    M : numpy 2d float array
+    """
     n_terms = M.shape[0]
-    n_docs = M.shape[1]
+    n_docs = float(M.shape[1])
     Mtfidf = np.zeros((n_terms, n_docs))
 
-    for doc in range(n_docs):
-        for term in range(n_terms):
-            tf = M[term, doc]/M[:, doc].sum()
-            idf = log(n_docs/M[term, :].sum(), 2)
-            Mtfidf[term, doc] = tf*idf
+    for term in range(n_terms):
+        dt = float(np.count_nonzero(M[term]))
+        Mtfidf[term] = log(n_docs/dt) if dt != 0 else 0
     return Mtfidf
+
+
+def tf(M):
+    """take matrix term-doc with frequencies and normalize with tf-idf instead
+
+    Arguments:
+    M : numpy 2d float array
+    """
+
+    n_terms = M.shape[0]
+    n_docs = M.shape[1]
+    Mtf = np.zeros((n_terms, n_docs))
+
+    for doc in range(n_docs):
+        # Mtf[:, doc] = M[:, doc]/M[:, doc].sum()
+        Mtf[:, doc] = M[:, doc]/M[:, doc].max()
+    return Mtf
 
 
 def scatter(U, V, labels):
@@ -63,9 +99,10 @@ def scatter(U, V, labels):
     for label, x, y in zip(labels, U, V):
         plt.annotate(
             label,
-            xy=(x, y),
+            xy=(x, y), xytext=(0.9, 9),
             textcoords="offset points", ha="right", va="bottom")
-    plt.show
+
+
 
 
 # def main():
@@ -73,62 +110,101 @@ def scatter(U, V, labels):
 # densier numpy array printing
 np.set_printoptions(precision=3)
 
-documents = [
-    "Human machine interface for lab abc computer applications",
-    "A survey of user opinion of computer system response time",
-    "The EPS user interface management system",
-    "System and human system engineering testing of EPS",
-    "Relation of user perceived response time to error measurement",
-    "The generation of random binary unordered trees",
-    "The intersection graph of paths in trees",
-    "Graph minors IV Widths of trees and well quasi ordering",
-    "Graph minors A survey"
-]
+# documents = [
+#     "Human machine interface for lab abc computer applications",
+#     "A survey of user opinion of computer system response time",
+#     "The EPS user interface management system",
+#     "System and human system engineering testing of EPS",
+#     "Relation of user perceived response time to error measurement",
+#     "The generation of random binary unordered trees",
+#     "The intersection graph of paths in trees",
+#     "Graph minors IV Widths of trees and well quasi ordering",
+#     "Graph minors A survey"
+# ]
+
+# docs_label = ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9"]
 
 stopwords = set(['for', 'a', 'of', 'the', 'and', 'to', 'in'])
 
-# terms = build_terms(documents, stopwords)
+#==========
+documents = [
+    "chat souris",
+    "chat souris animaux",
+    "souris clavier",
+    "clavier ordinateur"
+]
+docs_label = ["A1", "A2", "B1", "B2"]  # chat souris etc
 
+terms_label = build_terms(documents, stopwords)
+
+# ==========
 # using custom terms
-terms_label = ["human",
-         "interface",
-         "computer",
-         "user",
-         "system",
-         "response",
-         "time",
-         "EPS",
-         "survey",
-         "trees",
-         "graph",
-         "minors"]
+# terms_label = ["human",
+#          "interface",
+#          "computer",
+#          "user",
+#          "system",
+#          "response",
+#          "time",
+#          "EPS",
+#          "survey",
+#          "trees",
+#          "graph",
+#          "minors"]
+
+
+#==========
+# documents = [
+#     "A A A",
+#     "B B C A C",
+#     "A B A C",
+#     "A A A B",
+#     "X X Y",
+#     "Y Y X Z",
+#     "X X Z Z"
+# ]
+
+# terms_label = build_terms(documents, stopwords)
+
 
 terms = dict((key.lower(), value) for (value, key) in enumerate(terms_label))
 
 
 M = build_M(terms, documents)
-# MM = tfidf(M)
+# M = tfidf(M)
 
 U, s, V = np.linalg.svd(M)
 S = np.zeros(M.shape)
 
-# S[:s.size, :s.size] = np.diag(s)
+# # S[:s.size, :s.size] = np.diag(s)
 S[:s.size, :s.size] = np.diag([k if i < 3 else 0 for (i, k) in enumerate(s)])
 
-scatter(U[:, 1], U[:, 2], terms_label)
 
-# if np.allclose(M, np.dot(U, np.dot(S, V))):
-#     print "SVD OK"
+# scatter(U[:, 0], U[:, 1], terms_label)
+# scatter(V[:, 1], V[:, 2], docs_label)
 
-# print np.dot(U, np.dot(S, V))
+# # fig = plt.figure()
+# # ax = fig.add_subplot(111, projection='3d')
+# # ax.scatter(U[:, 0], U[:, 1], U[:, 2])
 
-# print ""
-# print "topic 1"
-# print [(i, j) for (i, j) in zip(np.dot(U, S)[:, 0], terms) if abs(i)>0.01]  # if abs(i) > 0.1]
-# print ""
-# print "topic 2"
-# print [(i, j) for (i, j) in zip(np.dot(U, S)[:, 1], terms) if abs(i)>0.01]  #if abs(i) > 0.1]
+plt.show()
+
+# # if np.allclose(M, np.dot(U, np.dot(S, V))):
+# #     print "SVD OK"
+
+# # print ""
+# # print "topic 1"
+# # print [(i, j) for (i, j) in zip(np.dot(U, S)[:, 0], terms) if abs(i)>0.01]  # if abs(i) > 0.1]
+# # print ""
+# # print "topic 2"
+# # print [(i, j) for (i, j) in zip(np.dot(U, S)[:, 1], terms) if abs(i)>0.01]  #if abs(i) > 0.1]
 
 
+# =======
 # if __name__ == '__main__':
 #     main()
+
+
+
+A = np.array([[0,1,0,0,1],[0,1,0,0,1],[3,2,2,0,1],[0,0,1,1,0],[0,0,1,1,1],[0,0,0,0,0]])
+A = A.astype(float)
